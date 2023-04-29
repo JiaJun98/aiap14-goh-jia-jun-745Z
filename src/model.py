5cdf3b6828c5c0e72193539d8755de475118dd38
JiaJun98 <gohjiajun.98@gmail.com>
2023-04-30 00:27:27 +0800

    Setting up YAML configurations, model and preprocessing class

---
+++ src/model.py
@@ -0,0 +1,5 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, roc_curve, auc, f1_score, confusion_matrix
---
+++ src/preprocess.py
@@ -0,0 +1,5 @@
+import sklearn.metrics
+from sklearn.preprocessing import OneHotEncoder,MinMaxScaler
+from imblearn.over_sampling import SMOTE
+import statsmodels.api as sma
+import joblib
