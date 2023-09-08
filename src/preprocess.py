f093d3ce0d9374b736a3697cfbb333fba4b223dc
Goh Jia Jun <gohjiajun98@Jodis-MacBook-Pro.local>
2023-09-08 19:23:04 +0800

    Testing

--- src/preprocess.py
+++ src/preprocess.py
@@ -8,7 +8,7 @@ from imblearn.over_sampling import SMOTE
 import numpy as np
 
 def check():
-    pass
+    print("Testing")
 
 def load_dataset(config_file):
     """Establishing connection with sqlite3
